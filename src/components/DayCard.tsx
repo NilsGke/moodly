f7559810f063cc9e8e3d64cbda131296b96de7f7
NilsGke <n.j.goeke@gmail.com>
2022-07-06 16:44:35 +0200

    organized moods to days

--- src/components/DayCard.tsx
+++ src/components/DayCard.tsx
@@ -1,21 +1,46 @@
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
 import "../styles/DayCard.scss";
+import MoodChart from "./MoodChart";
 
-export type day = {
+export type dayType = {
+    moods: Array<moodType>;
+    date: Date;
+};
+
+export type moodType = {
     id: React.Key;
     date: Date;
-    mood: 5 | 4 | 3 | 2 | 1;
+    time: Date;
+    mood: 5 | 4 | 3 | 2 | 1 | null;
     text: String;
 };
 
 type props = {
-    day: day;
+    day: dayType;
 };
 
 const DayCard: React.FC<props> = ({ day }) => {
     const date = new Date(day.date);
+
     return (
-        <div className="dayCard">
-            <h2 className="title">{date.toLocaleDateString()}</h2>
+        <div className="cardContainer">
+            <Link
+                to={"/days/" + date.toLocaleDateString()}
+                style={{ textDecoration: "none" }}
+            >
+                <div className="dayCard">
+                    <h2 className="title">
+                        {["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"].at(
+                            date.getDay()
+                        )}{" "}
+                        {date.toLocaleDateString()}
+                    </h2>
+                    <div className="chart">
+                        <MoodChart day={day} />
+                    </div>
+                </div>
+            </Link>
         </div>
     );
 };
