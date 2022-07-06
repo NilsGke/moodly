e2638e803bbd1c2db1b25de5730adedb16a55a57
NilsGke <n.j.goeke@gmail.com>
2022-07-06 16:45:19 +0200

    added functionality

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,15 +1,34 @@
-import DayCard, { day } from "./DayCard";
-
+import { useState } from "react";
+import DayCard, { dayType, moodType } from "./DayCard";
+import "../styles/CardList.scss";
 type props = {
-    days: Array<day>;
+    moods: Array<moodType>;
 };
 
-const CardList: React.FC<props> = ({ days }) => {
+const CardList: React.FC<props> = ({ moods }) => {
+    const days: Array<dayType> = [];
+    moods.forEach((mood) => {
+        const foundDay = days.find(
+            (day) =>
+                new Date(day.date).toDateString() ===
+                new Date(mood.date).toDateString()
+        );
+
+        if (foundDay !== undefined) {
+            foundDay.moods.push(mood);
+        } else {
+            days.push({
+                date: mood.date,
+                moods: [mood],
+            });
+        }
+    });
+
     return (
         <div id="cardContainer">
             <div id="cardList">
-                {days.map((mood) => (
-                    <DayCard key={mood.id} day={mood} />
+                {days.map((day, i) => (
+                    <DayCard key={i} day={day} />
                 ))}
             </div>
         </div>
