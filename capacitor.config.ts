d69e9815a9422985857d378e4655e575f7252aac
NilsGke <n.j.goeke@gmail.com>
2022-11-18 21:44:38 +0100

    new project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'ionic.moodly',
   appName: 'moodly',
   webDir: 'build',
   bundledWebRuntime: false
