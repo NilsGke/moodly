b91d3a07898b556d2e79c14ce3eb1798ba12ba52
NilsGke <n.j.goeke@gmail.com>
2022-11-22 16:10:48 +0100

    data now formatted

--- src/pages/Data.tsx
+++ src/pages/Data.tsx
@@ -1,15 +1,30 @@
 import { useEffect, useState } from "react";
 import { loadMoodsFromStorage } from "../helpers/moods";
+import "../styles/data.scss";
 
 const Data: React.FC = () => {
     const [data, setData] = useState<string | null>(null);
 
     useEffect(() => {
-        loadMoodsFromStorage().then((moods) => setData(JSON.stringify(moods)));
+        loadMoodsFromStorage().then((moods) =>
+            setData(JSON.stringify(moods, null, "    "))
+        );
     }, []);
 
     if (data === null) return <div className="loading">loading...</div>;
-    return <div className="data">{data}</div>;
+    return (
+        <div id="page" className="page data">
+            <button
+                className="copy"
+                onClick={() => navigator.clipboard.writeText(data)}
+            >
+                copy
+            </button>
+            <code lang="json">
+                <pre>{data}</pre>
+            </code>
+        </div>
+    );
 };
 
 export default Data;
