39e6301fe258a5801ddd716e575b91695988f121
NilsGke <n.j.goeke@gmail.com>
2022-11-28 22:28:51 +0100

    MoodListItem should expand so that all the content is visible #10

--- src/components/MoodListItem.tsx
+++ src/components/MoodListItem.tsx
@@ -3,6 +3,7 @@ import { moodColors, moodType, removeMood } from "../helpers/moods";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
 import dayjs from "dayjs";
+import { useRef } from "react";
 type props = {
     mood: moodType;
     activeMood: moodType["id"];
@@ -11,6 +12,8 @@ type props = {
     editMood: () => void;
 };
 
+const closedHeight = "65px";
+
 const MoodListItem: React.FC<props> = ({
     mood,
     activeMood,
@@ -18,11 +21,40 @@ const MoodListItem: React.FC<props> = ({
     refresh,
     editMood,
 }) => {
+    const ref = useRef<HTMLDivElement | null>(null);
+
     return (
         <div
+            ref={ref}
             key={mood.id as React.Key}
             className={"moodEntry" + (activeMood === mood.id ? " active" : "")}
-            onClick={() => setActiveMood(mood.id === activeMood ? -1 : mood.id)}
+            onClick={() => {
+                setActiveMood(mood.id === activeMood ? -1 : mood.id);
+                if (ref.current === undefined || ref.current === null) return;
+                if (mood.id === activeMood)
+                    ref.current.animate(
+                        [
+                            { maxHeight: ref.current.clientHeight + "px" },
+                            { maxHeight: closedHeight },
+                        ],
+                        {
+                            duration: 200,
+                            iterations: 1,
+                            fill: "both",
+                            easing: "ease-in-out",
+                        }
+                    );
+                else
+                    ref.current.animate(
+                        [{ maxHeight: closedHeight }, { maxHeight: "7000px" }],
+                        {
+                            duration: 2000,
+                            iterations: 1,
+                            fill: "both",
+                            easing: "ease-in-out",
+                        }
+                    );
+            }}
         >
             <span
                 className="moodNumber"
